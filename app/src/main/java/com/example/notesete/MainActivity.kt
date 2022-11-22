b108985a3aacd6fd6b4d0896cf6260cff962b174
Ritik Raj Sharma <Ritikrajsharma4321@gmail.com>
2022-11-22 22:06:56 +0530

    DONE

---
+++ app/src/main/java/com/example/notesete/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.example.notesete
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import android.view.Menu
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
+
+class MainActivity : AppCompatActivity(), NoteClickInterface, NoteClickDeleteInterface {
+
+    //on below line we are creating a variable for our recycler view, exit text, button and viewmodal.
+    lateinit var viewModal: NoteViewModal
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+    lateinit var noteRVAdapter: NoteRVAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //on below line we are initializing all our variables.
+        notesRV = findViewById(R.id.notesRV)
+        addFAB = findViewById(R.id.idFAB)
+        //on below line we are setting layout manager to our recycler view.
+        notesRV.layoutManager = LinearLayoutManager(this)
+        //on below line we are initializing our adapter class.
+        noteRVAdapter = NoteRVAdapter(this, this, this)
+        //on below line we are setting adapter to our recycler view.
+        notesRV.adapter = noteRVAdapter
+        //on below line we are initializing our view modal.
+        viewModal = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NoteViewModal::class.java)
+        //on below line we are calling all notes methof from our view modal class to observer the changes on list.
+        viewModal.allNotes.observe(this, Observer { list ->
+            list?.let {
+                //on below line we are updating our list.
+                noteRVAdapter.updateList(it)
+            }
+        })
+        addFAB.setOnClickListener {
+            //adding a click listner for fab button and opening a new intent to add a new note.
+            val intent = Intent(this@MainActivity, AddEditNoteActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+
+    override fun onNoteClick(note: Note) {
+        //opening a new intent and passing a data to it.
+        val intent = Intent(this@MainActivity, AddEditNoteActivity::class.java)
+        intent.putExtra("noteType", "Edit")
+        intent.putExtra("noteTitle", note.noteTitle)
+        intent.putExtra("noteDescription", note.noteDescription)
+        intent.putExtra("noteId", note.id)
+        startActivity(intent)
+        this.finish()
+    }
+
+    override fun onDeleteIconClick(note: Note) {
+        //in on note click method we are calling delete method from our viw modal to delete our not.
+        viewModal.deleteNote(note)
+        //displaying a toast message
+        Toast.makeText(this, "${note.noteTitle} Deleted", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.searchmenu, menu)
+
+        val searchItem = menu.findItem(R.id.actionSearch)
+
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filter(newText)
+                return false
+            }
+        })
+        return true
+    }
+
+    private fun filter(text: String) {
+        val filteredlist = mutableListOf<Note>()
+
+        for (item in viewModal.allNotes.value!!) {
+            if (item.getDescription().lowercase().contains(text.lowercase(Locale.getDefault()))) {
+                filteredlist.add(item)
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            viewModal.allNotes.observe(this, Observer { list ->
+                list?.let {
+                    //on below line we are updating our list.
+                    noteRVAdapter.updateList(filteredlist)
+                }
+            })
+        } else {
+            viewModal.allNotes.observe(this, Observer { list ->
+                list?.let {
+                    //on below line we are updating our list.
+                    noteRVAdapter.updateList(filteredlist)
+                }
+            })
+        }
+    }
+
+}
